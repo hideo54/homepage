ad92a3521c129133305ad9da053161404e288b16
hideo54 <hideo54@hideo54.com>
2016-01-01 04:15:28 +0900

    Happy New Year

---
+++ README.md
@@ -0,0 +1,26 @@
+# hideo54's Homepage
+
+## 概要
+
+https://hideo54.com のソース
+
+## バージョン
+
+Ver 4.0.0
+
+## 実行
+
+(必要であれば: `sudo ln -s /usr/bin/nodejs /usr/bin/node`)
+
+```bash
+npm install
+PORT=xxx ./bin/www
+```
+
+xxx番ポートで動作します。
+`PORT=xxx`表記を省略した場合、3000番ポートで動作します。
+
+## 連絡先
+
+* Twitter: @hideo54
+* E-mail: contact@hideo54.com
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+
+router.get('/activity', function(req, res, next) {
+    res.render('activity');
+});
+
+router.get('/links', function(req, res, next) {
+    res.render('links');
+});
+
+module.exports = router;
