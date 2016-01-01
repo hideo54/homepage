48e83499d6c53499dc43313e1a42dc00ce73aa3f
hideo54 <hideo54@hideo54.com>
2016-01-02 01:47:04 +0900

    Display title

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index');
+    res.render('index', {title: 'hideo54.com'});
 });
 
 router.get('/about', function(req, res, next) {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 router.get('/activity', function(req, res, next) {
-    res.render('activity');
+    res.render('activity', {title: 'Activity'});
 });
 
 router.get('/links', function(req, res, next) {
-    res.render('links');
+    res.render('links', {title: 'Links'});
 });
 
 module.exports = router;
