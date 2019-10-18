1185ca26a51c95dcc8001934b315d4a0d8b11fe6
hideo54 <hideo54@hideo54.com>
2019-10-19 00:39:59 +0900

    Make basic components and the top page model

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,18 @@
 import Head from 'next/head';
 import Header from './Header';
 
-export default ({ children, title = 'Default title', withHeader = true }) => (
+export default ({
+  children,
+  title = 'hideo',
+  description = title,
+  withHeader = true
+}) => (
   <>
     <Head>
       <title>{title}</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='description' content={description} />
     </Head>
     {withHeader && (
       <header>
