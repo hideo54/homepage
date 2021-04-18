73e4fd9b9dd17dbbe3b24158221407bfe66030f1
hideo54 <hideo54@hideo54.com>
2021-04-18 17:56:09 +0900

    Stop using yaml-loader and use getStaticProps instead

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 const withMDX = require('@next/mdx')();
 module.exports = withMDX({
-    webpack: (config, options) => {
-        config.module.rules.push({
-            test: /\.ya?ml$/,
-            type: 'json',
-            use: 'yaml-loader',
-        });
-        return config;
-    },
     pageExtensions: [ 'tsx', 'mdx' ],
 });
--- pages/accounts.tsx
+++ pages/accounts.tsx
@@ -1,26 +1,42 @@
+import { InferGetStaticPropsType, NextPage } from 'next';
+import fs from 'fs/promises';
+import yaml from 'yaml';
 import { Tag, TagProps } from '../src/components';
-import data from '../data.yaml';
 
-const accounts = data.accounts as {[key: string]: TagProps[]};
+interface Data {
+    accounts: {
+        [key: string]: TagProps[];
+    };
+}
 
-const tagGroups = Object.keys(accounts).map(category => (
-    <section key={category}>
-        <h2>{category}</h2>
-        {accounts[category].map(tag => (
-            <Tag
-                key={tag.name}
-                color={tag.color}
-                name={tag.name}
-                username={tag.username}
-                link={tag.link}
-            />
+export const getStaticProps = async () => {
+    const yamlStr = await fs.readFile('data.yaml', 'utf-8');
+    const data = yaml.parse(yamlStr) as Data;
+    return {
+        props: { data },
+    };
+};
+
+type StaticProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const main: NextPage<StaticProps> = ({ data }) => (
+    <>
+        <h1>アカウント一覧</h1>
+        {Object.keys(data.accounts).map(category => (
+            <section key={category}>
+                <h2>{category}</h2>
+                {data.accounts[category].map(tag => (
+                    <Tag
+                        key={tag.name}
+                        color={tag.color}
+                        name={tag.name}
+                        username={tag.username}
+                        link={tag.link}
+                    />
+                ))}
+            </section>
         ))}
-    </section>
-));
-
-const main = () => <>
-    <h1>アカウント一覧</h1>
-    {tagGroups}
-</>;
+    </>
+);
 
 export default main;
