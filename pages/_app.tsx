a2ca4f470f6b2584f527ae96c9fb2f729974a590
Hideo Yasumoto <hideo54@hideo54.com>
2023-02-13 13:19:35 +0900

    Fix CSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ const GlobalStyle = createGlobalStyle`
 
     h1, h2, h3, h4, h5, h6, p, div {
         margin-top: 0;
-        color: '#333333';
+        color: #333333;
 
         @media (prefers-color-scheme: dark) {
             color: #EEEEEE;
