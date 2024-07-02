e97f11f087e080adc606df05aa9c16162b75f126
Hideo Yasumoto <hideo54@hideo54.com>
2024-07-02 22:54:44 +0900

    情報量を増やしていい感じに

--- pages/karaoke.tsx
+++ pages/karaoke.tsx
@@ -1,37 +1,90 @@
 import dynamic from 'next/dynamic';
 import dayjs from 'dayjs';
 import { groupBy } from 'lodash';
+import clsx from 'clsx';
+import { MusicalNote } from '@styled-icons/ionicons-solid'
 import Layout from '../components/Layout';
 import damScoresDataJson from '../lib/dam-scores.json';
 import { sortBy } from '../lib/utils';
 
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false });
 
+const ScoreWithAverage: React.FC<{
+    myScore: number;
+    averageScore: number;
+    label?: string;
+}> = ({ myScore, averageScore, label }) => (
+    <div>
+        <div className='text-sm font-bold -mb-1'>
+            {label}
+        </div>
+        <span className={clsx([
+            'mr-2',
+            label ? 'text-xl font-bold' : 'text-2xl font-black',
+        ])}>
+            {myScore.toFixed(label ? 0 : 3 )}
+        </span>
+        <span>
+            平均 {myScore >= averageScore && '+'}{(myScore - averageScore).toFixed(label ? 0 : 3)}
+        </span>
+    </div>
+);
+
 const Score: React.FC<{
-    title: string;
-    artistName: string;
-    performKey: string;
-    score: number;
-}> = ({ title, artistName, performKey, score}) => (
-    <li className='block'>
-        <div>
-            <span className='text-lg font-bold mr-2'>
-                {title}
-            </span>
-            {performKey !== '0' &&
-                <div className='inline-block mr-2 px-1  border border-solid border-neutral-600 rounded-lg'>
-                    {performKey}
-                </div>
-            }
-            <div className='inline-block'>
-                <span className='pr-2'>
-                    /
+    scoreData: typeof damScoresDataJson[0];
+}> = ({ scoreData }) => (
+    <li className='block mb-8'>
+        <div className='flex'>
+            <div>
+                <MusicalNote size={48} className='mt-1' />
+            </div>
+            <div>
+                <span className='text-lg font-bold mr-2'>
+                    {scoreData.contentsName}
                 </span>
-                <span>{artistName}</span>
+                {scoreData.lastPerformKey !== '0' &&
+                    <div className='inline-block mr-2 px-1 border border-solid border-neutral-600 rounded-lg'>
+                        {scoreData.lastPerformKey}
+                    </div>
+                }
+                <div className='inline-block'>
+                    <span className='pr-2'>
+                        /
+                    </span>
+                    <span>{scoreData.artistName}</span>
+                </div>
+                <ScoreWithAverage
+                    myScore={Number(scoreData['#text']) / 1000}
+                    averageScore={Number(scoreData.nationalAverageTotalPoints) / 1000}
+                />
             </div>
         </div>
-        <div className='text-xl font-black'>
-            {score}
+        <div className='ml-5 pl-3 border-l-2 border-solid border-neutral-400'>
+            <ScoreWithAverage
+                myScore={Number(scoreData.radarChartPitch)}
+                averageScore={Number(scoreData.nationalAveragePitch)}
+                label='音程'
+            />
+            <ScoreWithAverage
+                myScore={Number(scoreData.radarChartVibratoLongtone)}
+                averageScore={Number(scoreData.nationalAverageVibratoAndLongtone)}
+                label='ビブラート&ロングトーン'
+            />
+            <ScoreWithAverage
+                myScore={Number(scoreData.radarChartRhythm)}
+                averageScore={Number(scoreData.nationalAverageRhythm)}
+                label='リズム'
+            />
+            <ScoreWithAverage
+                myScore={Number(scoreData.radarChartExpressive)}
+                averageScore={Number(scoreData.nationalAverageExpression)}
+                label='表現力'
+            />
+            <ScoreWithAverage
+                myScore={Number(scoreData.radarChartStability)}
+                averageScore={Number(scoreData.nationalAverageStability)}
+                label='安定性'
+            />
         </div>
     </li>
 );
@@ -50,13 +103,9 @@ const App = () => {
                     ).map(score =>
                         <Score
                             key={score.scoringDateTime}
-                            title={score.contentsName}
-                            artistName={score.artistName}
-                            performKey={score.lastPerformKey}
-                            score={Number(score['#text']) / 1000}
+                            scoreData={score}
                         />
-                    )
-                    }
+                    )}
                 </ul>
             </section>
             <section>
