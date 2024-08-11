7644cc0e9c748dbec899a80418a621bce874a367
Hideo Yasumoto <hideo54@hideo54.com>
2024-08-11 15:35:58 +0900

    複数回の取引に対応

--- scripts/getMattocoAllCountryGainHistory.ts
+++ scripts/getMattocoAllCountryGainHistory.ts
@@ -5,8 +5,11 @@ import dayjs from 'dayjs';
 // Hard coded because...
 // * mattoco has severe login system which often requires email 2FA
 // * The number of shares held does (and should!) merely change
-const hold_from = '20240701';
-const n_hold = 113_533;
+const holdHistory = {
+    '20240617': 78_046,
+    '20240624': 0,
+    '20240701': 113_533,
+};
 
 const main = async () => {
     const { data } = await axios.get<{
@@ -15,13 +18,21 @@ const main = async () => {
             BASE_PRICE: number;
         }[];
     }>('https://direct.mattoco.jp/chartdata/chart_data_renewal_253425.js');
-    const priceHistoryAfterHold = data.ROWS.filter(row => row.BASE_DATE >= hold_from);
-    const priceWhenHold = priceHistoryAfterHold[0].BASE_PRICE;
-    const gainHistoryAfterHold = priceHistoryAfterHold.map(row => ({
-        date: dayjs(row.BASE_DATE, 'YYYYMMDD').format('YYYY-MM-DD'),
-        gain: (row.BASE_PRICE - priceWhenHold) * n_hold / 10_000,
-    }));
-    await fs.writeFile('lib/mattoco-all-country-gain-history.json', JSON.stringify(gainHistoryAfterHold));
+    const holdFrom = Object.keys(holdHistory).sort()[0];
+    const priceHistoryAfterHold = data.ROWS.filter(row => row.BASE_DATE >= holdFrom);
+    const gainHistory = priceHistoryAfterHold.slice(1).reduce((acc, cur, i) => {
+        const newObj = {
+            date: dayjs(cur.BASE_DATE, 'YYYYMMDD').format('YYYY-MM-DD'),
+            gain: acc[acc.length - 1].gain
+                + (priceHistoryAfterHold[i + 1].BASE_PRICE - priceHistoryAfterHold[i].BASE_PRICE)
+                * Object.entries(holdHistory).findLast(([date]) => date < cur.BASE_DATE)![1] / 10_000,
+        };
+        return [...acc, newObj];
+    }, [{
+        date: dayjs(priceHistoryAfterHold[0].BASE_DATE, 'YYYYMMDD').format('YYYY-MM-DD'),
+        gain: 0,
+    }]);
+    await fs.writeFile('lib/mattoco-all-country-gain-history.json', JSON.stringify(gainHistory));
 };
 
 main();
