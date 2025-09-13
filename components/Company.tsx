bca51239820755c931b0c3c9dcdd186db09725e5
Hideo Yasumoto <hideo54@hideo54.com>
2025-09-13 15:43:01 +0900

    Separate component

---
+++ components/Company.tsx
@@ -0,0 +1,14 @@
+const Company = () => (
+    <span
+        className='inline-block w-28 h-4 mx-1 bg-neutral-600 rounded align-[-2px]'
+    >
+        <span className='absolute text-[0px]'>
+            (ひみつ)
+        </span>
+        <span className='hidden'>
+            {'HIDEO54{F0R_TH3_CUR10US:_N1KK31_1NC}'}
+        </span>
+    </span>
+);
+
+export default Company;
